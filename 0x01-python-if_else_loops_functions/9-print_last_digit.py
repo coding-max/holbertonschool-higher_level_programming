8127a15c2978f392ef5dc159cf7d6b5e357d86f2
Maximilinao <maximiliano.pan@hotmail.com>
2021-04-27 16:37:14 -0300

    modf: condition for nagative digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,9 @@
 def print_last_digit(number):
     if number < 0:
         aux = -number
-    
+
     last_digit = aux % 10
-    if number < 0
+    if number < 0:
         last_digit = -last_digit
-    
+
     return(last_digit)
