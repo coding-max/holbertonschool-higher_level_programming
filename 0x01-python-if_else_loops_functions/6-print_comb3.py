b840f2fb8d01a8c904b7d6eb4b7b676344c4c2d7
Maximilinao <maximiliano.pan@hotmail.com>
2021-04-27 15:03:42 -0300

    updated task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for tens in range(0, 10):
+for tens in range(0, 9):
     for ones in range(tens + 1, 10):
-        print("{}{}, ".format(tens, ones), end='')
+        if tens != 8:
+            print("{}{}, ".format(tens, ones), end='')
 print("89")
