8613840ee838497ac68180665afecd7314ca2a19
Maximilinao <maximiliano.pan@hotmail.com>
2021-05-04 15:18:46 -0300

    updated task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,6 +3,6 @@ def replace_in_list(my_list, idx, element):
     "replaces an element of a list at a specific position."
 
     "if idx is negative or out of range, return None"
-    if idx > 0 or idx < len(my_list):
+    if idx > 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
