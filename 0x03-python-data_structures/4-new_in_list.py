7665b255e49e71b4bd9edee022553f89032dbba9
Maximilinao <maximiliano.pan@hotmail.com>
2021-05-04 15:23:57 -0300

    updated task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,7 @@ def new_in_list(my_list, idx, element):
 
     my_list_copy = my_list[:]
     "if idx is negative or out of range, return a copy of the original list"
-    if idx > 0 and idx < len(my_list):
-        my_list_copy[idx] = element
+    if idx < 0 or idx >= len(my_list):
+        return (my_list_copy)
+    my_list_copy[idx] = element
     return (my_list_copy)
