36ec47d8447d86e942b01507ea745f3d72b6e82e
Maximilinao <maximiliano.pan@hotmail.com>
2021-05-06 14:09:01 -0300

    updated task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@ def multiply_by_2(a_dictionary):
 
     b_dictionary = {}
     for i in a_dictionary:
-        b_dictionary[i] = a_dictionary[i]
+        b_dictionary[i] = a_dictionary[i] * 2
     return (b_dictionary)
