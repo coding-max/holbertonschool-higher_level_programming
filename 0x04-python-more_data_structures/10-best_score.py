11623dd98c4ac3a726f182697530912e0fcf4f0d
Maximilinao <maximiliano.pan@hotmail.com>
2021-05-06 14:07:34 -0300

    updated task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
     "returns a key with the biggest integer value"
 
     biggest_value = 0
     biggest_key = None
-    for key, value in a_dictionary.items():
-        if value > biggest_value:
-            biggest_value = value
-            biggest_key = key
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if value > biggest_value:
+                biggest_value = value
+                biggest_key = key
     return biggest_key
