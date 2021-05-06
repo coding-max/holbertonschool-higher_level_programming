43902904f8466466f569967a7817a3c767d9b3e2
Maximilinao <maximiliano.pan@hotmail.com>
2021-05-06 14:07:21 -0300

    updated task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,5 @@
 def update_dictionary(a_dictionary, key, value):
     "replaces or adds key/value in a dictionary"
 
-    return (a_dictionary[key] = value)
+    a_dictionary[key] = value
+    return (a_dictionary)
