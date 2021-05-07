2de745bef9b93d20c98793ccfb581d731ee5431c
Maximilinao <maximiliano.pan@hotmail.com>
2021-05-06 23:27:01 -0300

    added task 12 (ft elcomix)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    "converts a roman numeral to an integer"
+
+    if type(roman_string) is not str or roman_string is None:
+        return (0)
+    total = 0
+    r_dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for idx in range(len(roman_string)):
+        i = r_dic.get(roman_string[idx])
+        if (idx < len(roman_string) - 1):
+            ii = r_dic.get(roman_string[idx + 1])
+            if ii > i:
+                total -= i
+            else:
+                total += i
+        else:
+            total += i
+    return (total)
