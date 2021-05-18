280ee541642fe5f6aaa9cdaf5c4f8299d8621575
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-17 23:19:08 -0300

    updated tasks 7-8 (redirect to stderr)

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_integer_err(value):
     "prints an integer with error message"
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError) as err:
-        print("Exception: {}".format(err))
+    except (TypeError, ValueError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import sys
+
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except (TypeError, ValueError, IndexError, ZeroDivisionError) as error:
-        print("Exception: {}".format(error))
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
