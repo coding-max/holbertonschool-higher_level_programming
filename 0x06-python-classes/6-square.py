0a631219365f4caead113e895aade464128b9676
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-18 22:58:57 -0300

    updated task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,18 +7,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Instantiation with optional size and optional position"""
-        "Check size"
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        "Check position"
-        if type(position) != tuple:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
