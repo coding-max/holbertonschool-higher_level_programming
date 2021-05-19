754989df6a7c6e10f52e48637f4e5bc2b0965616
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-18 21:39:32 -0300

    updated task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Square with size"""
+
+
 class Square:
+    """Representation of a square"""
+
     def __init__(self, size=0):
+        """Instantiation with optional size"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
