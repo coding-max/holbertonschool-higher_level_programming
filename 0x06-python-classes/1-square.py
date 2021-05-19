2609262e3569e84391f4b50d288f67bc0a0cfbf4
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-18 21:37:28 -0300

    updated task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Square with size"""
+
+
 class Square:
+    """Representation of a square"""
+
     def __init__(self, size):
+        """Instantiation with size"""
         self.__size = size
