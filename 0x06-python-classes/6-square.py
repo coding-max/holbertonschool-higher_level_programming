4af81c435e4d58e55afb303e968293b042ea06d1
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-18 21:21:11 -0300

    added tasks 0-6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if ((type(value) != tuple) or (len(value) != 2) or
+                not all((type(number) != int) for number in value) or
+                not all(number >= 0 for number in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in  range(self.__size):
+                print("#", end="")
+            print("")
+        if self.__size == 0:
+            print("")
