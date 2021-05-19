77f9ce00d104b26881d98a083ae268c1c7eb2b2e
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-18 21:48:23 -0300

    updated task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Square with size"""
+
+
 class Square:
+    """Representation of a square"""
+
     def __init__(self, size=0):
+        """Instantiation with optional size"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -10,10 +16,12 @@ class Square:
 
     @property
     def size(self):
+        """Property to retrieve size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """Property setter to set size"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -22,10 +30,12 @@ class Square:
 
     @property
     def position(self):
+        """Property to retrieve position"""
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        """Property setter to set position"""
         if ((type(value) != tuple) or (len(value) != 2) or
                 not all((type(number) != int) for number in value) or
                 not all(number >= 0 for number in value)):
@@ -33,9 +43,11 @@ class Square:
         self.__position = value
 
     def area(self):
+        """Public instance method that returns the current square area"""
         return self.__size ** 2
 
     def my_print(self):
+        """Public instance method that prints the square with char #"""
         for i in range(self.__size):
             for j in  range(self.__size):
                 print("#", end="")
