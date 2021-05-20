de47bc8e4bd4f67653a4520ffb220eede73c9bea
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-19 21:52:57 -0300

    updated task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,12 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """Instantiation with optional size"""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
     @property
     def size(self):
