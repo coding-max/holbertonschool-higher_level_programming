81caa4907db34584bf3087411d6167293140d331
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-19 22:32:07 -0300

    added task 102

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,14 +1,52 @@
 #!/usr/bin/python3
+"""Square with size"""
 
 
+class Square:
+    """Representation of a square"""
 
-    def __str__(self):
-        """Define the print() representation of a Square."""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+    def __init__(self, size=0):
+        """Instantiation with optional size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Property to retrieve size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Property setter to set size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        """Method to compare using == operator"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Method to compare using != operator"""
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        """Method to compare using > operator"""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Method to compare using >= operator"""
+        return self.area() >= other.area()
+
+    def __lt__(self, other):
+        """Method to compare using < operator"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Method to compare using <= operator"""
+        return self.area() <= other.area()
