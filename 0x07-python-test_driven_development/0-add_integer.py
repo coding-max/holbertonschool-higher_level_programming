58c21a3aa9c4ac5bef9c56b686f264c7170c1def
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-05-22 02:43:28 -0300

    updated task 0 - missing one argument

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,9 +4,9 @@
 
 def add_integer(a, b=98):
     "adds 2 integers"
-    if type(a) != int and type(a) != float:
+    if a is None or type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    elif type(b) != int and type(b) != float:
+    elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
