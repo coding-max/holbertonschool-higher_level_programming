4928ef26337c5631d6956b7d1d016ffb79172a35
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-01 22:03:17 -0300

    deleted tmp file

--- 0x0A-python-inheritance/tmp.py
+++
@@ -1,6 +0,0 @@
-"""
-python3 -c 'print(__import__("1-my_list").__doc__)'
-python3 -c 'print(__import__("1-my_list").MyClass.__doc__)'
-python3 -c 'print(__import__("1-my_list").my_function.__doc__)'
-python3 -c 'print(__import__("1-my_list").MyClass.my_function.__doc__)'
-"""
