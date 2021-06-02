f7e945382103dcb83c90c79c5393526ed756666b
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-01 22:02:02 -0300

    updated test file (case with only negatives)

---
+++ 0x0A-python-inheritance/tmp.py
@@ -0,0 +1,6 @@
+"""
+python3 -c 'print(__import__("1-my_list").__doc__)'
+python3 -c 'print(__import__("1-my_list").MyClass.__doc__)'
+python3 -c 'print(__import__("1-my_list").my_function.__doc__)'
+python3 -c 'print(__import__("1-my_list").MyClass.my_function.__doc__)'
+"""
