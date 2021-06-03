c9f32c9753364c88af3388a17e7ab75b5b37dff0
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-02 23:05:44 -0300

    added task 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Search and update"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each line with a spc. string"""
+    with open(filename, "r") as file:
+        for line in file:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as file:
+        file.write(text)
