856f5e00d5de2ad491901bfd133aae56b06f9f5e
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-02 23:09:31 -0300

    updated task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,6 +4,7 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """inserts a line of text to a file, after each line with a spc. string"""
+    text = ""
     with open(filename, "r") as file:
         for line in file:
             text += line
