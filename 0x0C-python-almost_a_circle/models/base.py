ec52edff19e9793949eac29c6ed7c141a78a0fdb
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-05 17:36:57 -0300

    added task 1 (class 'Base')

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module of 'Base' class"""
+
+
+class Base:
+    """Representation of a Base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
