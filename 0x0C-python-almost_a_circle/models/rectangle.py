8411aec22a6e4c9eb3ab75e8edebca5a9d82c0cd
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-06-05 17:37:20 -0300

    added tasks 2-9 (class 'Rectangle')

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+"""module of 'Rectangle' class"""
+
+from .base import Base
+
+
+class Rectangle(Base):
+    """Representation of a Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """class constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        """custom __str__ method for Rectangle"""
+        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
+                self.id, self.x, self.y, self.width, self.height)
+
+    @property
+    def width(self):
+        """width getter method"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width setter method"""
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter method"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height setter method"""
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter method"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter method"""
+        if (type(value) is not int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter method"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter method"""
+        if (type(value) is not int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+
+    def area(self):
+        """returns the area value of the Rectangle instance"""
+        return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character '#' """
+        for jump in range(self.y):
+            print()
+        for row in range(self.height):
+            for space in range(self.x):
+                print(" ", end="")    
+            for column in range(self.width):
+                print("#", end="")
+            print()
+
+    def update(self, *args, **kwargs):
+        """updates values of the Rectangle instance from args or kwargs"""
+        if args is not None and len(args) > 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.width = arg
+                elif i == 2:
+                    self.height = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+                i += 1
+        elif kwargs is not None:
+            for (key, value) in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
