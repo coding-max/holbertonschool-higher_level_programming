d88b0c0f7bdf04f4f797063eb15b9dd9dbb84be1
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-07-14 02:11:13 +0000

    updated task

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -6,7 +6,7 @@
     -- The database name will be passed as an argument of the mysql command
 
 SELECT tv_shows.title AS title
-FROM tv_show_genres INNER JOIN tv_genres
+FROM tv_show_genres INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_genres.id
 INNER JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id AND tv_genres.name = 'Comedy'
