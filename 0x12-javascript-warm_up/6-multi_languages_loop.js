312872882c4bec09816169aefacd9164e26bd406
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-02 22:43:05 +0000

    updated task 6 (searching the bug ft. R08S & Mishan)

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < 3; i++) {
-  console.log(array[i]);
+  console.log(arr[i]);
 }
