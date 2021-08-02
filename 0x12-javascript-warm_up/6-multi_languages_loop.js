1cb768375527c61c1598be96ce0ee6939d36c7cb
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-02 22:51:41 +0000

    updated task 6 (all for a capital letter)

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(arr[i]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i in array) {
+  console.log(array[i]);
 }
