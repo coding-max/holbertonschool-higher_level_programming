9c9ccf83224375bf837999d090278bcd913764bc
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-02 20:21:19 +0000

    updated task 11 (semistandard)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv.length > 3) {
-  const args = process.argv.map(Number)
-  args.slice(2)
+  const args = process.argv.map(Number);
+  args.slice(2);
   args.sort((a, b) => a - b);
   console.log(args[args.length - 2]);
 } else {
