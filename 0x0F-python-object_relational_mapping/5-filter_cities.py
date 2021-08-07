8505a11614a9723bb0db66fcf9b58fb68e65e953
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-08-07 18:22:19 +0000

    updated task 5 (pep8 style)

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""takes in the name of a state as an argument and 
+"""takes in the name of a state as an argument and
    lists all cities of that state, using the database 'hbtn_0e_4_usa'"""
 
 import MySQLdb
@@ -10,13 +10,14 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("""SELECT cities.name
-                      FROM cities JOIN states
-                      ON cities.state_id = states.id
-                      WHERE states.name = %(state)s""",
-                      {'state': argv[4]})
+                   FROM cities JOIN states ON cities.state_id = states.id
+                   WHERE states.name = %(state)s""", {'state': argv[4]})
     result = cursor.fetchall()
-    for i in range(len(result)):
-        if i < len(result) - 1:
+    lenght = len(result)
+    if lenght == 0:
+        print('')
+    for i in range(lenght):
+        if i < lenght - 1:
             print(result[i][0], end=', ')
         else:
             print(result[i][0])
