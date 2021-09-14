29143221a4dfb379ecabbf26b74bc611f54d7048
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-09-14 14:17:37 +0000

    333#0 updated script

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,9 +6,6 @@
 */
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', function (error, data) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(data);
-  }
+  if (error) console.log(error);
+  else console.log(data);
 });
